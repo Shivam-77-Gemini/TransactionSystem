2179603615ebcd87e3417f918269c083b69fe0d0
Shivam-77-Gemini <Shivam.Tiwari@Geminisolutions.com>
2022-05-18 17:07:51 +0530

    Comit!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TranscationsComponent } from './transcations/transcations.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TranscationsComponent
   ],
   imports: [
     BrowserModule,
